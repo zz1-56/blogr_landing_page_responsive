39191b8634be06e6c8ff1d64c21f6295d1fda504
zaid <falwzyr5@gmail.com>
2026-02-03 15:56:13 +0300

    added poup for desktop

---
+++ script.js
@@ -0,0 +1,16 @@
+var image = document.querySelector("#row");
+var navbar = document.querySelector("#popup");
+var button = document.querySelector("#contact");
+
+button.onclick = function() {
+    if (navbar.style.display === "none"){
+        navbar.style.display = 'block'
+        image.style.transform = 'rotate(180deg)'
+        
+    }     
+    else{
+        navbar.style.display = 'none'
+        image.style.transform = 'rotate(0deg)'
+        
+    }
+}
